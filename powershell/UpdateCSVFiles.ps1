47be4b549b00e3f5dca6929c7f2e24137bd04885
Edwin Panameno <Edwin.Panameno@med.usc.edu>
2020-05-11 12:01:36 -0700

    added script to update npi for providers

---
+++ powershell/UpdateCSVFiles.ps1
@@ -0,0 +1,22 @@
+﻿## This script iterates thru each csv file and add the NPI number
+## as a new column to each csv file. The results of each operation 
+## is then stored in the results folder (this is what will be sent
+## to Phynd).
+###  powershell -ExecutionPolicy Bypass .\UpdateCSVFiles.ps1
+
+
+$csvFiles = Get-ChildItem .\*.csv
+$resultsFolder = "C:\Users\panameno\Desktop\Kyruus-ClinicalKeyword Data\results\"
+$count = 0
+
+foreach($file in $csvFiles) {
+    $fileInfo = Get-Item $file  
+    $outputFilePath = "C:\Users\panameno\Desktop\Kyruus-ClinicalKeyword Data\results\$($fileInfo.Name)"
+   
+    write-output "Processing file number $count - $($fileInfo.Name)"
+
+    Import-Csv $file  | 
+    Select-Object *,@{Name="NPI";Expression={$fileInfo.BaseName}} | 
+    Export-Csv $outputFilePath -NoTypeInformation 
+    $count++
+ }
